dfecbf80cef41688d77be8b921424ef59730a58a
LaraSLGomes <larastephanny05@gmail.com>
2026-05-20 18:34:46 -0300

    organizando pastas e arquivos

---
+++ app/ReadBoxdApp.java
@@ -0,0 +1,8 @@
+package app;
+
+public class ReadBoxdApp {
+    public void iniciarLoopDeComandos() {
+        System.out.println("Bem-vindo ao ReadBoxd!");
+        System.out.println("Aqui você pode implementar o loop de comandos da aplicação.");
+    }
+}
