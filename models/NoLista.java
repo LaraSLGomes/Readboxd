0f78e8f0f992d870fcabfba8a26ee842b8e65bf1
LARA STEPHANNY LIMA GOMES <951552887@adm.unifor.br>
2026-05-21 09:01:30 -0300

    feat: implementa entidade Livro e as classes de Nós das estruturas

--- models/NoLista.java
+++ models/NoLista.java
@@ -1,4 +1,23 @@
 package models;
+
 public class NoLista {
-    
+    public Livro livro;      
+    private NoLista proximo;  
+
+    public NoLista(Livro libro) {
+        this.livro = libro;
+        this.proximo = null;
+    }
+
+    public NoLista getProximo() {
+        return this.proximo;
+    }
+
+    public void setProximo(NoLista proximo) {
+        this.proximo = proximo;
+    }
+
+    public boolean temProximo() {
+        return this.proximo != null;
+    }
 }
